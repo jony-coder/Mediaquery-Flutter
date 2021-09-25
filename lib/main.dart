a9e55729e2023d569f2fa06fe0d87be05d1e416e
jony-coder <jahid.jony.cse@gmail.com>
2021-09-25 22:59:29 +0600

    MediaQuery

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(debugShowCheckedModeBanner: false, home: HomePage());
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Center(
+          child: Container(
+            height: MediaQuery.of(context).size.height/3,
+            width: MediaQuery.of(context).size.width/3,
+            color: Colors.deepOrange,
+          ),
+        ),
+      ),
+    );
+  }
+}
